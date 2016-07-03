543650d5d55b152bf58246b28a52fe9ad495e429
Luke Floden <robodomo1@gmail.com>
2016-07-03 12:01:40 -0400

    mouseleave fadeto from slow to fast

--- script.js
+++ script.js
@@ -54,43 +54,43 @@ $(document).ready(function(){
 	});
 
 	$("#gmail").mouseleave(function(){
-		$("#gmail").fadeTo('slow',.5);
+		$("#gmail").fadeTo('fast',.5);
 	});
 	$("#outlook").mouseleave(function(){
-		$("#outlook").fadeTo('slow',.5);
+		$("#outlook").fadeTo('fast',.5);
 	});
 	$("#yahoo").mouseleave(function(){
-		$("#yahoo").fadeTo('slow',.5);
+		$("#yahoo").fadeTo('fast',.5);
 	});
 	$("#youtube").mouseleave(function(){
-		$("#youtube").fadeTo('slow',.5);
+		$("#youtube").fadeTo('fast',.5);
 	});
 	$("#netflix").mouseleave(function(){
-		$("#netflix").fadeTo('slow',.5);
+		$("#netflix").fadeTo('fast',.5);
 	});
 	$("#drive").mouseleave(function(){
-		$("#drive").fadeTo('slow',.5);
+		$("#drive").fadeTo('fast',.5);
 	});
 	$("#photos").mouseleave(function(){
-		$("#photos").fadeTo('slow',.5);
+		$("#photos").fadeTo('fast',.5);
 	});
 	$("#docs").mouseleave(function(){
-		$("#docs").fadeTo('slow',.5);
+		$("#docs").fadeTo('fast',.5);
 	});
 	$("#keep").mouseleave(function(){
-		$("#keep").fadeTo('slow',.5);
+		$("#keep").fadeTo('fast',.5);
 	});
 	$("#github").mouseleave(function(){
-		$("#github").fadeTo('slow',.5);
+		$("#github").fadeTo('fast',.5);
 	});
 	$("#wordpress").mouseleave(function(){
-		$("#wordpress").fadeTo('slow',.5);
+		$("#wordpress").fadeTo('fast',.5);
 	});
 	$("#codecademy").mouseleave(function(){
-		$("#codecademy").fadeTo('slow',.5);
+		$("#codecademy").fadeTo('fast',.5);
 	});
 	$("#forums").mouseleave(function(){
-		$("#forums").fadeTo('slow',.5);
+		$("#forums").fadeTo('fast',.5);
 	});
 
 	/*$('#links').mouseleave(function(){
