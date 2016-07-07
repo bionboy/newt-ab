301a6673c5f530fb25cc0f63c4a48bd0b73a3d88
Luke Floden <robodomo1@gmail.com>
2016-07-07 12:46:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,101 +1,13 @@
 $(document).ready(function(){
-	$("#gmail").fadeTo('fast',.5);
-	$("#outlook").fadeTo('fast',.5);
-    $("#yahoo").fadeTo('fast',.5);
-    $("#youtube").fadeTo('fast',.5);
-    $("#netflix").fadeTo('fast',.5);
-    $("#drive").fadeTo('fast',.5);
-    $("#photos").fadeTo('fast',.5);
-    $("#docs").fadeTo('fast',.5);
-    $("#keep").fadeTo('fast',.5);
-    $("#github").fadeTo('fast',.5);
-    $("#wordpress").fadeTo('fast',.5);
-    $("#codecademy").fadeTo('fast',.5);
-    $("#forums").fadeTo('fast',.5);
+	$(".icon").fadeTo('fast',.5);
 
-
-	$("#gmail").mouseenter(function(){
-		$("#gmail").fadeTo('fast',1);
+	$(".icon").mouseenter(function(){
+		$(this).fadeTo('fast',1);
 	});
-	$("#outlook").mouseenter(function(){
-		$("#outlook").fadeTo('fast',1);
-	});
-	$("#yahoo").mouseenter(function(){
-		$("#yahoo").fadeTo('fast',1);
-	});
-	$("#youtube").mouseenter(function(){
-		$("#youtube").fadeTo('fast',1);
-	});
-	$("#netflix").mouseenter(function(){
-		$("#netflix").fadeTo('fast',1);
-	});
-	$("#drive").mouseenter(function(){
-		$("#drive").fadeTo('fast',1);
-	});
-	$("#photos").mouseenter(function(){
-		$("#photos").fadeTo('fast',1);
-	});
-	$("#docs").mouseenter(function(){
-		$("#docs").fadeTo('fast',1);
-	});
-	$("#keep").mouseenter(function(){
-		$("#keep").fadeTo('fast',1);
-	});
-	$("#github").mouseenter(function(){
-		$("#github").fadeTo('fast',1);
-	});
-	$("#wordpress").mouseenter(function(){
-		$("#wordpress").fadeTo('fast',1);
-	});
-	$("#codecademy").mouseenter(function(){
-		$("#codecademy").fadeTo('fast',1);
-	});
-	$("#forums").mouseenter(function(){
-		$("#forums").fadeTo('fast',1);
+	$(".icon").mouseleave(function(){
+		$(this).fadeTo('fast',.5);
 	});
 
-	$("#gmail").mouseleave(function(){
-		$("#gmail").fadeTo('fast',.5);
-	});
-	$("#outlook").mouseleave(function(){
-		$("#outlook").fadeTo('fast',.5);
-	});
-	$("#yahoo").mouseleave(function(){
-		$("#yahoo").fadeTo('fast',.5);
-	});
-	$("#youtube").mouseleave(function(){
-		$("#youtube").fadeTo('fast',.5);
-	});
-	$("#netflix").mouseleave(function(){
-		$("#netflix").fadeTo('fast',.5);
-	});
-	$("#drive").mouseleave(function(){
-		$("#drive").fadeTo('fast',.5);
-	});
-	$("#photos").mouseleave(function(){
-		$("#photos").fadeTo('fast',.5);
-	});
-	$("#docs").mouseleave(function(){
-		$("#docs").fadeTo('fast',.5);
-	});
-	$("#keep").mouseleave(function(){
-		$("#keep").fadeTo('fast',.5);
-	});
-	$("#github").mouseleave(function(){
-		$("#github").fadeTo('fast',.5);
-	});
-	$("#wordpress").mouseleave(function(){
-		$("#wordpress").fadeTo('fast',.5);
-	});
-	$("#codecademy").mouseleave(function(){
-		$("#codecademy").fadeTo('fast',.5);
-	});
-	$("#forums").mouseleave(function(){
-		$("#forums").fadeTo('fast',.5);
-	});
-
-
-	// $("#hackernews").slideup('fast');
 	$("#hackernewsbutton").click(function(){
 		$("#hackernews").slideToggle('slow');
 	});
@@ -109,4 +21,12 @@ $(document).ready(function(){
     $("#forums").click(function(){
 		$("#forumlinks").slideToggle('slow');
 	});
+
+	var time = new Date().getHours();
+	console.log(time);
+	if(time >= 21){
+		$("body").css('background-image','url("backgrounds/background-night.jpg")');
+	} else{
+		$("body").css('background-image','url("backgrounds/background.jpg")');
+	}
 });
