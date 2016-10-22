a55d16c5509031a009aa154c0643372a48a74f0f
Luke Floden <robodomo1@gmail.com>
2016-10-22 11:58:32 -0400

    clean up and image resize

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
 $(document).ready(function(){
-	$('#hackernews').draggable();
-	$('#youtubefeed').draggable();
-	$('#quorafeed').draggable();
-
-	$(".icon").fadeTo('fast',.5);
+	$(".icon").fadeTo('fast',.7);
 
 	$(".icon").mouseenter(function(){
 		$(this).fadeTo('fast',1);
 	});
 	$(".icon").mouseleave(function(){
-		$(this).fadeTo('fast',.5);
+		$(this).fadeTo('fast',.7);
 	});
-
-	$("#hackernewsbutton").click(function(){
-		$("#hackernews").slideToggle('slow');
-	});
-	$("#youtubefeedbutton").click(function(){
-		$("#youtubefeed").slideToggle('slow');
-	});
-	$("#quorafeedbutton").click(function(){
-		$("#quorafeed").slideToggle('slow');
-	});
-
+	//$(".icon").click(function() {
+  		//$(this).toggle("puff");
+	//});
     $("#forums").click(function(){
 		$("#forumlinks").slideToggle('slow');
 	});
 
-	var time = new Date().getHours();
-	console.log(time);
-	if(time >= 21){
+	var hours = new Date().getHours();
+	var mins = new Date().getMinutes();
+	console.log(hours);
+	console.log(mins);
+	if(hours >= 21){
 		$("body").css('background-image','url("backgrounds/red-background-night.jpg")');
 		$("#links").addClass('night-links');
 		$("#forumlinks").addClass('night-links');
-	} else{
+	}
+	else{
 		$("body").css('background-image','url("backgrounds/background.jpg")');          // also make a change so that you can see red icons at night and maybe blue out red icons. 
 	}
 });
